8ec0fdfeb565e46951ef55b59306d88ca85c32d1
lucafmarques <lucafmarqs@gmail.com>
2024-04-10 14:51:07 -0300

    feat: Cover all native Go types

--- examples/example_1.go
+++ examples/example_1.go
@@ -10,4 +10,5 @@ func main() {
 	fmt.Println(env.Get[string]("LOG_FORMAT", "INFO"))
 	fmt.Println(env.MustGet[string]("STRING"))
 	fmt.Println(env.MustGet[int]("INTEGER"))
+	fmt.Println(env.MustGet[[]string]("SLICE"))
 }
